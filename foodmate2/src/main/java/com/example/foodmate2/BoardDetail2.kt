aa59aca26bb0ca312ffafbf8ebcade79b92744d4
hanhojjung <ghwjd9269@naver.com>
2023-06-13 12:38:34 +0900

    230613 3번째

---
+++ foodmate2/src/main/java/com/example/foodmate2/BoardDetail2.kt
@@ -0,0 +1,11 @@
+package com.example.foodmate2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+// 비작성자 전용 Board Detail
+class BoardDetail2 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_board_detail2)
+    }
+}
