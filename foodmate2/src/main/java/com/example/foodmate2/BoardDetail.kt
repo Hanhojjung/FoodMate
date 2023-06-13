625f57055eadcf876484dfabe2900aa87bc58e2f
hanhojjung <ghwjd9269@naver.com>
2023-06-13 11:28:15 +0900

    230613 2번째

--- foodmate2/src/main/java/com/example/foodmate2/BoardDetail.kt
+++ foodmate2/src/main/java/com/example/foodmate2/BoardDetail.kt
@@ -1,5 +1,6 @@
 package com.example.foodmate2
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -11,11 +12,24 @@ class BoardDetail : AppCompatActivity() {
         setContentView(R.layout.activity_board_detail)
 
         val regList: Button = findViewById(R.id.reg_list)
+        val regUpdate: Button = findViewById(R.id.reg_update)
+        val regDelete: Button = findViewById(R.id.reg_delete)
 
         regList.setOnClickListener {
-            // 등록하기 버튼 클릭 시 처리할 로직 작성
-            // 예: BoardList로 이동
+            // 목록 버튼으로 클릭 시 MainActivity로 이동
             val intent = Intent(this@BoardDetail, MainActivity::class.java)
+            startActivity(intent)
+        }
+
+        regUpdate.setOnClickListener {
+            // 예: 수정 버튼을 클릭 시 BoardUpdate로 이동
+            val intent = Intent(this@BoardDetail, BoardUpdate::class.java)
+            startActivity(intent)
+        }
+
+        regDelete.setOnClickListener {
+            // 예: 수정 버튼을 클릭 시 BoardDelete로 이동
+            val intent = Intent(this@BoardDetail, BoardDelete::class.java)
             startActivity(intent)
         }
     }
