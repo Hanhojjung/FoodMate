f1fb42366b098846bb968f8258d3cd65e5870df9
hanhojjung <ghwjd9269@naver.com>
2023-06-15 12:35:42 +0900

    230615 3번째(food3 통합)

---
+++ foodmate2/src/main/java/com/example/foodmate2/controller/controller/Board.kt
@@ -0,0 +1,10 @@
+package com.example.foodmate2.controller.controller
+
+import com.google.gson.annotations.SerializedName
+data class Board(
+    val title: String,
+    val user_count: String,
+    val bar_name: String,
+    val bar_appoint: String,
+    val content: String
+)
