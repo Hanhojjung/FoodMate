b92838c8ccfb711fed212e1dc0701d2d75ec334d
hanhojjung <ghwjd9269@naver.com>
2023-06-16 16:04:31 +0900

    230616 8번째(food3 통합 : 시간 오류 고침+ 메인에 리스트 불러오기)

--- foodmate3/src/main/java/com/example/foodmate3/fragment/HomeFragment.kt
+++ foodmate3/src/main/java/com/example/foodmate3/fragment/HomeFragment.kt
@@ -1,60 +1,75 @@
 package com.example.foodmate3.fragment
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.foodmate3.R
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.foodmate3.adapter.BoardAdapter
+import com.example.foodmate3.controller.BoardController
+import com.example.foodmate3.databinding.FragmentHomeBinding
+import com.example.foodmate3.model.BoardDto
+import com.example.foodmate3.network.RetrofitBuilder
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.HttpException
+import retrofit2.Response
+import java.io.IOException
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    private lateinit var binding: FragmentHomeBinding
+    private lateinit var boardService: BoardController
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var boardAdapter: BoardAdapter
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        boardService = RetrofitBuilder.BoardListService()
+
+        recyclerView = binding.recyclerView
+        recyclerView.layoutManager = LinearLayoutManager(requireContext())
+
+        val boardList: MutableList<BoardDto> = mutableListOf()
+        boardAdapter = BoardAdapter(requireContext(), boardList)
+        recyclerView.adapter = boardAdapter
+
+        getBoardList(boardService)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    private fun getBoardList(boardService: BoardController) {
+        val boardListCall: Call<List<BoardDto>> = boardService.getAllBoard()
+
+        boardListCall.enqueue(object : Callback<List<BoardDto>> {
+            override fun onResponse(call: Call<List<BoardDto>>, response: Response<List<BoardDto>>) {
+                if (response.isSuccessful) {
+                    val boardListResponse = response.body()
+                    boardListResponse?.let {
+                        boardAdapter.setData(it)
+                    }
+                } else {
+                    Log.e("HomeFragment", "Error: ${response.code()}")
                 }
             }
+
+            override fun onFailure(call: Call<List<BoardDto>>, t: Throwable) {
+                if (t is IOException) {
+                    Log.e("HomeFragment", "Network Error: ${t.message}")
+                } else if (t is HttpException) {
+                    Log.e("HomeFragment", "HTTP Error: ${t.code()}")
+                } else {
+                    Log.e("HomeFragment", "Error: ${t.message}")
+                }
+            }
+        })
     }
 }
