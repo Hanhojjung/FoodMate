b2cde6ee38b92aac48675ac9d2516024124785b8
hanhojjung <ghwjd9269@naver.com>
2023-06-20 15:26:33 +0900

    230620 3 번째(food3 통합 : 메모 Read(전체회원) 완료)

--- foodmate3/src/main/java/com/example/foodmate3/controller/TodoController.kt
+++ foodmate3/src/main/java/com/example/foodmate3/controller/TodoController.kt
@@ -4,10 +4,14 @@ import com.example.foodmate3.model.TodoDto
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
 
 interface TodoController {
 
     @POST("/insertTodo")
     fun insertTodo(@Body todo: TodoDto): Call<ResponseBody>
+
+    @GET("/todoList")
+    fun getAllTodo(): Call<List<TodoDto>>
 }
--- foodmate3/src/main/java/com/example/foodmate3/fragment/CalendarFragment.kt
+++ foodmate3/src/main/java/com/example/foodmate3/fragment/CalendarFragment.kt
@@ -14,16 +14,23 @@ import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.example.foodmate3.MainActivity
 import com.example.foodmate3.R
+import com.example.foodmate3.adapter.BoardAdapter
+import com.example.foodmate3.adapter.TodoAdapter
+import com.example.foodmate3.controller.BoardController
 import com.example.foodmate3.controller.SharedPreferencesUtil
 import com.example.foodmate3.controller.TodoController
+import com.example.foodmate3.model.BoardDto
 import com.example.foodmate3.model.TodoDto
 import com.example.foodmate3.network.RetrofitBuilder
 import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.HttpException
 import retrofit2.Response
+import java.io.IOException
 
 
 class CalendarFragment : Fragment() {
@@ -31,9 +38,10 @@ class CalendarFragment : Fragment() {
 
     private lateinit var todoService: TodoController
     private lateinit var memoplus: ImageButton
-    private lateinit var saveButton : Button
     private lateinit var titleEditText: EditText
     private lateinit var memoEditText: EditText
+    private lateinit var todoAdapter : TodoAdapter
+    private lateinit var recyclerView: RecyclerView
 
     @SuppressLint("MissingInflatedId")
     override fun onCreateView(
@@ -50,9 +58,45 @@ class CalendarFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+
+        recyclerView = view.findViewById(R.id.recyclerView)
+
+        val todoList: MutableList<TodoDto> = mutableListOf()
+        todoAdapter = TodoAdapter(requireContext(), todoList)
+        recyclerView.adapter = todoAdapter
+
+        getBoardList(todoService)
+
         memoplus.setOnClickListener {
             showMemoDialog()
         }
+    }
+
+    private fun getBoardList(todoService: TodoController) {
+        val boardListCall: Call<List<TodoDto>> = todoService.getAllTodo()
+
+        boardListCall.enqueue(object : Callback<List<TodoDto>> {
+            override fun onResponse(call: Call<List<TodoDto>>, response: Response<List<TodoDto>>) {
+                if (response.isSuccessful) {
+                    val boardListResponse = response.body()
+                    boardListResponse?.let {
+                        todoAdapter.setData(it)
+                    }
+                } else {
+                    Log.e("TodoFragment", "Error: ${response.code()}")
+                }
+            }
+
+            override fun onFailure(call: Call<List<TodoDto>>, t: Throwable) {
+                if (t is IOException) {
+                    Log.e("TodoFragment", "Network Error: ${t.message}")
+                } else if (t is HttpException) {
+                    Log.e("TodoFragment", "HTTP Error: ${t.code()}")
+                } else {
+                    Log.e("TodoFragment", "Error: ${t.message}")
+                }
+            }
+        })
     }
 
     private fun showMemoDialog() {
